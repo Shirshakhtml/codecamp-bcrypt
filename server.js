0a0e7c236d5843c2bde680c7f03bf753df7ad7fa
Shirshakhtml <shirshak.roy@learner.manipal.edu>
2023-06-03 00:57:22 +0530

    Main changes - BCrypt part 3

--- server.js
+++ server.js
@@ -9,16 +9,18 @@ const saltRounds = 12;
 const myPlaintextPassword = 'sUperpassw0rd!';
 const someOtherPlaintextPassword = 'pass123';
 
-
-//START_ASYNC -do not remove notes, place code between correct pair of notes.
-
 bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
     console.log(hash);
     bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
       console.log(res);
     });
   });
+//START_ASYNC -do not remove notes, place code between correct pair of notes.
 
+let hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+console.log(hash);
+let result = bcrypt.compareSync(myPlaintextPassword, hash);
+console.log(result);
 //END_ASYNC
 
 //START_SYNC
